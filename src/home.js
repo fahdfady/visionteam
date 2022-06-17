9fded0656dc6802ee4ddd41799ba943a8d4bc641
Ahmed <ahmedazmycs@protonmail.com>
2022-06-17 15:56:06 +0200

    fixed bugs

--- src/home.js
+++ src/home.js
@@ -4,35 +4,32 @@ import Teams from "./Teamspreview";
 import useFetch from "./hooks/useFetch";
 
 const Home = () => {
-    const { data: results, isPending, error } = useFetch("http://localhost:3001/results");
-    const { data: esportteams } = useFetch("http://localhost:3001/esportTeams");
+  const { data: results, isPending, error } = useFetch("/results");
+  const { data: esportteams } = useFetch("/esportTeams");
 
+  return (
+    <div className='home'>
+      <section className='banner'>
+        <div className='banner-container'>
+          <div className='layer'>
+            <h2>
+              <span> Vis</span>ion
+            </h2>
 
-    return (
-        <div className="home">
-            <section className="banner">
-                <div className="banner-container">
-                    <div className="layer">
-                        <h2>
-                            <span> Vis</span>ion
-                        </h2>
-
-                        <p>The vision is always clear</p>
-                    </div>
-                </div>
-            </section>
-
-
-            { error && <div>{ error }</div> }
-
-            { isPending && <div>Loading...</div> }
-
-            { results && <Results results={ results } /> }
-
-            { esportteams && <Teams teams={ esportteams } /> }
-
+            <p>The vision is always clear</p>
+          </div>
         </div>
-    )
-}
+      </section>
+
+      {error && <div>{error}</div>}
+
+      {isPending && <div>Loading...</div>}
+
+      {results && <Results results={results} />}
+
+      {esportteams && <Teams teams={esportteams} />}
+    </div>
+  );
+};
 
 export default Home;
